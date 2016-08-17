19f0d42eba0ebb97dab9e4efbb61b55520559e38
PhazzaT <piodul@op.pl>
2016-08-17 11:31:04 +0200

    Made the project C++11 again

--- src/MakeUnique.hpp
+++ src/MakeUnique.hpp
@@ -5,14 +5,18 @@
 
 #include <memory>
 
-#if defined(__cplusplus) && __cplusplus >= 201402L
+#if defined(__cplusplus) && __cplusplus < 201402L
 
 // A Herb Sutter's placeholder implementation of make_unique
+
+namespace std {
 
 template<typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args&& ...args)
 {
 	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
 }
 
 #endif // defined(__cplusplus) && __cplusplus >= 201402L
