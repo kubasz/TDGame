2029da071e5125701c398f6288a7f15f7bdc3bdf
krzys-h <krzys_h@interia.pl>
2016-08-22 17:31:26 +0200

    Some balancing tweaks to new level

--- tools/generate-level.py
+++ tools/generate-level.py
@@ -6,25 +6,33 @@ level = {
 	'starting-lives': 10,
 	'starting-money': 100,
 	'grid-size': [16, 16],
-	'goal':	[ 8, 8 ],
+	'goal':	[8, 8],
 	'waves': []
 }
 
 time = 3
 for i in range(10):
+	creepdata = { 
+		'type': 'GenericCreep',
+		'hp': 40*(i+1),
+		'bounty': 10*(i+1),
+		'spawn-at': [0, 0],
+		'spawn-time': {
+			'start': 0.0,
+			'count': 5*(i+1),
+			'interval': 2.0,
+		}
+	}
+	def creeps_at(data, pos):
+		creepdata2 = data.copy()
+		creepdata2['spawn-at'] = pos
+		return creepdata2
 	level['waves'].append({
 		'start-time': time,
-		'creeps': [{
-			'type': 'GenericCreep',
-			'hp': 40*(i+1),
-			'bounty': 10*(i+1),
-			'spawn-at': [0, 0],
-			'spawn-time': {
-				'start': 0.0,
-				'count': 5*(i+1),
-				'interval': 2.0,
-			}
-		}]
+		'creeps': [
+			creeps_at(creepdata, [0, 0]),
+			creeps_at(creepdata, [15, 15])
+		]
 	})
 	time += 5*(i+1)*2+10
 
